03d364dec87e0356387dd678c257e2420e6a552e
unknown <gufrankhursheed7@gmail.com>
2025-02-14 18:53:07 +0530

    add login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,6 +64,10 @@ export default async function Home() {
       <div className="h-screen flex items-center justify-center flex-col">
         <h1 className="text-4xl font-bold">Welcome to the admin of the website</h1>
         <p className="text-2xl font-medium my-2 ">An account is needed to view this page</p>
+        <Link href="/login" className="rounded border border-gray-400 bg-gray-400 px-10 py-2 my-2 text-base font-medium text-white hover:bg-transparent hover:text-gray-400 focus:outline-none focus:ring active:text-gray-400">
+          Log in
+        </Link>
+        <h2 className="text-center my-1">OR</h2>
         <form
           action={async () => {
             "use server"
