52bb5482dd0a4cbe22364d2a34daf99be76aca4d
unknown <gufrankhursheed7@gmail.com>
2025-02-15 15:53:20 +0530

    add header currentUser

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,14 @@
-import { auth } from "@/auth";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { User } from "@auth/core/types";
+import { useAuth } from "../context/AuthContext";
 
-export default async function Header() {
-    const session = await auth()
-    const user = session?.user
+export default function Header({ user }: { user?: User }) {
+    const { currentUser } = useAuth();
 
-    if(session){
+    if (user || currentUser) {
         return (
             <header className="bg-white border-b sticky z-50 top-0">
                 <div className="mx-auto max-w-screen px-4 sm:px-6 lg:px-8">
@@ -17,11 +19,11 @@ export default async function Header() {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
                                 </svg>
-    
+
                             </Link>
                             <h1 className="text-2xl md:text-xl text-teal-600 hover:cursor-pointer">Admin</h1>
                         </div>
-    
+
                         <div className="md:flex md:items-center md:gap-12">
                             <nav aria-label="Global" className="hidden md:block">
                                 <ul className="flex items-center gap-6 text-lg">
@@ -41,14 +43,22 @@ export default async function Header() {
                                     </li>
                                 </ul>
                             </nav>
-    
+
                             <div className="flex items-center gap-4">
                                 <div className="sm:flex sm:gap-4">
-                                    <div className="h-10 w-10">
-                                        <Image width={1000} height={1000} className="h-9 w-9 rounded-full object-cover object-center" src={user?.image ?? ""} alt="" />
-                                    </div>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        width="2em"
+                                        height="2em"
+                                    >
+                                        <path
+                                            fill="currentColor"
+                                            d="M6.196 17.485q1.275-.918 2.706-1.451Q10.332 15.5 12 15.5t3.098.534t2.706 1.45q.99-1.025 1.593-2.42Q20 13.667 20 12q0-3.325-2.337-5.663T12 4T6.337 6.338T4 12q0 1.667.603 3.064q.603 1.396 1.593 2.42M12 12.5q-1.263 0-2.132-.868T9 9.5t.868-2.132T12 6.5t2.132.868T15 9.5t-.868 2.132T12 12.5m0 8.5q-1.883 0-3.525-.701t-2.858-1.916t-1.916-2.858T3 12t.701-3.525t1.916-2.858q1.216-1.215 2.858-1.916T12 3t3.525.701t2.858 1.916t1.916 2.858T21 12t-.701 3.525t-1.916 2.858q-1.216 1.215-2.858 1.916T12 21"
+                                        ></path>
+                                    </svg>
                                 </div>
-    
+
                                 <div className="block md:hidden">
                                     <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
                                         <svg
